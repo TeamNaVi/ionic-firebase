1122f2231c74cd10cc83649cef95701d39644655
SeongUk52 <rkfka7849@gmail.com>
2022-04-25 00:54:05 +0900

    Revert "업로드 기능 구현"
    This reverts commit cc6a0070f185a5b345f0904a08cf8370878d42de.

--- src/app/dashboard/upload/upload.module.ts
+++ src/app/dashboard/upload/upload.module.ts
@@ -6,24 +6,15 @@ import { IonicModule } from '@ionic/angular';
 
 import { UploadPageRoutingModule } from './upload-routing.module';
 
-import {AngularFirestore} from '@angular/fire/compat/firestore';
 import { UploadPage } from './upload.page';
-import {AngularFireStorageModule} from '@angular/fire/compat/storage';
-import {BrowserModule} from '@angular/platform-browser';
-import {AngularFireModule} from '@angular/fire/compat';
-import {environment} from '../../../environments/environment';
 
 @NgModule({
   imports: [
-
     CommonModule,
     FormsModule,
     IonicModule,
-    UploadPageRoutingModule,
-    AngularFireStorageModule,
-    AngularFireModule.initializeApp(environment.firebase), // firebase 모듈 사용
+    UploadPageRoutingModule
   ],
-  declarations: [UploadPage],
-  bootstrap: [UploadPage]
+  declarations: [UploadPage]
 })
 export class UploadPageModule {}
