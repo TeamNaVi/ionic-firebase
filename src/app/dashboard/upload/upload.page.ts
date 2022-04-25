45faee19deab5d621a6cffebc58fe5c7bdb4f8c1
SeongUk52 <rkfka7849@gmail.com>
2022-04-25 18:15:00 +0900

    업로드 진행 바 추가

--- src/app/dashboard/upload/upload.page.ts
+++ src/app/dashboard/upload/upload.page.ts
@@ -2,21 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { DashboardPage } from '../dashboard.page';
 
-import {AngularFireStorage} from '@angular/fire/compat/storage';
+import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} from '@angular/fire/compat/storage';
 import {AuthenticationService} from '../../services/authentication.service';
 import {AngularFirestore} from '@angular/fire/compat/firestore';
 import firebase from 'firebase/compat/app';
+import {Observable} from "rxjs";
 @Component({
   selector: 'app-upload',
   templateUrl: './upload.page.html',
   styleUrls: ['./upload.page.scss'],
 })
 export class UploadPage implements OnInit {
+  private ref: AngularFireStorageReference;
+  private task: AngularFireUploadTask;
+  private uploadProgress: Observable<number | undefined>;
   constructor(private navCtrl: NavController, private afStorage: AngularFireStorage, private afs:  AngularFirestore) {}
   ngOnInit() {  }
   upload(event) {
-    // eslint-disable-next-line max-len
-    this.afStorage.upload('/learning-model-files/'+ firebase.auth().currentUser.uid +'/' + event.target.files[0].name, event.target.files[0]);
+    const filePath = '/learning-model-files/'+ firebase.auth().currentUser.uid +'/' + event.target.files[0].name;
+
+    this.ref = this.afStorage.ref(filePath);
+    this.task = this.ref.put(event.target.files[0]);
+    this.uploadProgress = this.task.percentageChanges();
+
   }
   goToDashboardPage() {
     this.navCtrl.navigateBack('/dashboard');
