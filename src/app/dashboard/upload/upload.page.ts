4813ef480d818731e3c5365677dd0ebb445f2416
sori <smcdidcksthf@naver.com>
2022-04-26 00:44:18 +0900

    patch: progressBar01

--- src/app/dashboard/upload/upload.page.ts
+++ src/app/dashboard/upload/upload.page.ts
@@ -2,11 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import { DashboardPage } from '../dashboard.page';
 
-import {AngularFireStorage, AngularFireStorageReference, AngularFireUploadTask} from '@angular/fire/compat/storage';
-import {AuthenticationService} from '../../services/authentication.service';
-import {AngularFirestore} from '@angular/fire/compat/firestore';
+import {
+  AngularFireStorage,
+  AngularFireStorageReference,
+  AngularFireUploadTask,
+} from '@angular/fire/compat/storage';
+import { AuthenticationService } from '../../services/authentication.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 import firebase from 'firebase/compat/app';
-import {Observable} from "rxjs";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-upload',
@@ -17,20 +21,26 @@ export class UploadPage implements OnInit {
   private ref: AngularFireStorageReference;
   private task: AngularFireUploadTask;
   private uploadProgress: Observable<number | undefined>;
-  constructor(private navCtrl: NavController, private afStorage: AngularFireStorage, private afs:  AngularFirestore) {}
-  ngOnInit() {  }
+  constructor(
+    private navCtrl: NavController,
+    private afStorage: AngularFireStorage,
+    private afs: AngularFirestore
+  ) {}
+  ngOnInit() {}
   upload(event) {
-    const filePath = '/learning-model-files/'+ firebase.auth().currentUser.uid +'/' + event.target.files[0].name;
+    const filePath =
+      '/learning-model-files/' +
+      firebase.auth().currentUser.uid +
+      '/' +
+      event.target.files[0].name;
 
     this.ref = this.afStorage.ref(filePath);
     this.task = this.ref.put(event.target.files[0]);
     this.uploadProgress = this.task.percentageChanges();
-
   }
   goToDashboardPage() {
     this.navCtrl.navigateBack('/dashboard');
   }
 }
-
 
 //push test
