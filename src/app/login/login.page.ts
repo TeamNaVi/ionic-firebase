42fafe04658b46cbe16216cc63a98d697a06706c
sori <smcdidcksthf@naver.com>
2022-04-27 21:49:50 +0900

    newlogin01

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -54,6 +54,23 @@ export class LoginPage implements OnInit {
     ],
   };
 
+  signInBtn = document.getElementById('signIn');
+  signUpBtn = document.getElementById('signUp');
+  fistForm = document.getElementById('form1');
+  secondForm = document.getElementById('form2');
+  container = document.querySelector('.container');
+
+  // signInBtn.addEventListener("click", () => {
+  //   container.classList.remove("right-panel-active");
+  // });
+
+  // signUpBtn.addEventListener("click", () => {
+  //   container.classList.add("right-panel-active");
+  // });
+
+  // fistForm.addEventListener("submit", (e) => e.preventDefault());
+  // secondForm.addEventListener("submit", (e) => e.preventDefault());
+
   loginUser(value) {
     this.authService.loginUser(value).then(
       (res) => {
